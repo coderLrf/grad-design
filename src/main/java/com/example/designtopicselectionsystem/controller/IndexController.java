8e3254fb9b15f5ec870992b024f3a504e4e8fc88
luoruofei <2099769572@qq.com>
2021-06-24 10:01:19 +0800

    commit index

--- src/main/java/com/example/designtopicselectionsystem/controller/IndexController.java
+++ src/main/java/com/example/designtopicselectionsystem/controller/IndexController.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping("/admin")
+    @GetMapping({"/admin", "/"})
     public String toIndex() {
         return "redirect:admin/index";
     }
